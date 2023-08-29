7b95d6b3bf4b59bdd41aec4e5faa33b71bdc40ee
RobertoEduardo1232 <143459561+RobertoEduardo1232@users.noreply.github.com>
2023-08-29 00:10:43 +0000

    hola

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+int main(int argc, char const *argv[])
+{
+    int a = 0;
+    for (int contador = 0; contador < 10; contador++)
+
+    {
+        a += 1; /* code */
+    }
+    /* code */
+    return 0;
+    
+}
