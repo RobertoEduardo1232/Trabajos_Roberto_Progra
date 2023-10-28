31860fe25e731392657a0ce0c3d83a20e27b63c3
RobertoEduardo1232 <143459561+RobertoEduardo1232@users.noreply.github.com>
2023-10-28 02:40:04 +0000

    TrabajoFinalizadoParcial2

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,52 @@
+#include <iostream>
+#include <Mascota.hpp>
+
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    int a = 0;
-    for (int contador = 0; contador < 10; contador++)
+    // Mascota m1;
+    // Mascota m2;
 
-    {
-        a += 1; /* code */
-    }
-    /* code */
+    // m1.Comer();
+    // m2.Comer();
+
+    cout << "Tipos de datos" << endl;
+    cout << "int" << sizeof(int) << endl;
+    cout << "char" << sizeof(char) << endl;
+    cout << "bool" << sizeof(bool) << endl;
+    cout << "float" << sizeof(float) << endl;
+    cout << "double" << sizeof(double) << endl;
+    cout << "Mascota" << sizeof(Mascota) << endl;
+
+    int a = 4;
+    cout << "Direcciones de memoria" << endl;
+    cout << "tamaño" << sizeof(a) << endl;
+    cout << "contenido" << a << endl;
+    cout << "direccion" << &a << endl;
+
+    void *puntero = malloc(32);
+    free(puntero);
+
+    cout << "Direcciones de memoria" << endl;
+    cout << "size of: " << sizeof(puntero) << endl;
+    cout << "puntero1:" << puntero << endl;
+    cout << "puntero2:" << &puntero << endl;
+
+    // memoria dinámica en C
+    cout << "Mascota en C: " << endl;
+    Mascota *MascotaC = (Mascota *)malloc(sizeof(Mascota));
+    MascotaC->Inicializar();
+    MascotaC->DecirNombre();
+    MascotaC->Destruir();
+    free(MascotaC);
+
+    // cout << "Tamaño Puntero: " << sizeof(Mascota) << endl;
+    //  Memoria dinámica en C++
+    cout << "Mascota en C++: " << endl;
+    Mascota *MascotaCPP = new Mascota();
+    MascotaCPP->DecirNombre();
+    delete MascotaCPP;
+    // std::cout<<"I changed de time zone, but what do i know"<<std::endl;
     return 0;
-    
 }
